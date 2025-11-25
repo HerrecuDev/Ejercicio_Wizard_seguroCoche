56d1ffec15f7cfc1b5d32774b298d1f88662e182
HerrecuDev <phercue2805@g.educaand.es>
2025-11-25 14:44:40 +0100

    ejercicio casi completo

---
+++ src/main/java/org/iesdm/wizardcoche/WizardCocheApplication.java
@@ -0,0 +1,13 @@
+package org.iesdm.wizardcoche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardCocheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WizardCocheApplication.class, args);
+    }
+
+}
